c8a98d7483c54aec1a8a84ee29840e84023ca38c
UHeeJoon <yhj3855@naver.com>
2021-11-20 19:45:22 +0900

    7단계

--- 백준 문제 풀이/11720번.cpp
+++ 백준 문제 풀이/11720번.cpp
@@ -1,12 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int num = 0, sum = 0;
+	cin >> num;
+	char num_array[101] = {0};
+	cin >> num_array;
+	for (int i = 0; i < num; i++) 
+		sum += num_array[i] - '0';
+	cout << sum;
+
+}
+/* C¾ð¾î
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 int main() {
-	int a, sum = 0; scanf("%d", &a);
+	int a, sum = 0; 
+	scanf("%d", &a);
 	char* A = (char*)malloc(sizeof(char) * a);
 	for (int i = 0; i <= a; i++)
 		scanf("%c", &A[i]);
 	for (int i = 1; i <= a; i++)
-		sum += A[i] - 48;
+		sum += A[i] - '0';
 	printf("%d\n", sum);
 }
+*/
