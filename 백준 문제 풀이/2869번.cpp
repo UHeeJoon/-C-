e63e3092e054bc7507b9154e6cc01563782e9c28
UHeeJoon <yhj3855@naver.com>
2021-12-03 20:07:50 +0900

    백준 8단계 문제

--- 백준 문제 풀이/2869번.cpp
+++ 백준 문제 풀이/2869번.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main() {
 	int a, b, c; cin >> a >> b >> c;
-	(c - a) % (a - b) != 0 ? cout << (c - a) / (a - b) + 2 : cout << (c - a) / (a - b) + 1;
+	(c - a) % (a - b) != 0 ? 
+		cout << (c - a) / (a - b) + 2 : 
+		cout << (c - a) / (a - b) + 1;
+
 	// (1). c - a 는 마지막 날엔 미끄러지지 않으니  -a를 하고  결과 값에 +1
 	// (2). a - b 는 하루에 이동한 거리 
 	// (조건 a) (c - a) % (a - b) != 0는 /를 했을때 나머지 값의 유무에 따라 1이 낮아지기 때문에 고려
