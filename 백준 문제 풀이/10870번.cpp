06ffd67d9fd04dfa3e2ffb89ccf60d5fa8dab6b0
UHeeJoon <yhj3855@naver.com>
2021-12-10 20:38:49 +0900

    백준 10단계 문제

--- 백준 문제 풀이/10870번.cpp
+++ 백준 문제 풀이/10870번.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 int cnt = 0;
 int Fibonacci(int num) {
-	int fibo1 = 0, fibo2 = 1, fibo3 = fibo1 + fibo2;
-
-	cnt++;
-	if (cnt == num)
-		return fibo3;
-	return 0;
+    if (num == 0)
+        return 0;
+    if (num == 1)
+        return 1;
+    return Fibonacci(num - 2) + Fibonacci(num - 1);
 }
 int main() {
 	int num; cin >> num;
---
+++ 백준 문제 풀이/2477번.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void starSquare(int N) {
+	int a;
+
+}
+int main() {
+	int N; cin >> N;
+	starSquare(N / 3);
+}
