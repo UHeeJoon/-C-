38dded2e2779e0def92f5a0af8557761111d2167
UHeeJoon <yhj3855@naver.com>
2022-01-03 20:58:09 +0900

    코테 공부

---
+++ 백준 문제 풀이/2309번.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int nanj[10];
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	for (int i = 0; i < 9; i++) cin >> nanj[i];
+	sort(nanj, nanj + 9);
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += nanj[i];
+		if (sum == 100)break;
+	} while (next_permutation(nanj, nanj + 9));
+	for (int i = 0; i < 7; i++) cout << nanj[i] << "\n";
+}
