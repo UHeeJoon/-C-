daf7ddf21d1dc68bedf1573bdc4b87e235ecbab3
UHeeJoon <yhj3855@naver.com>
2022-01-14 00:11:32 +0900

    12단계 문제 풀이

--- 백준 문제 풀이/18870번.cpp
+++ 백준 문제 풀이/18870번.cpp
@@ -1,37 +1,45 @@
 #include<iostream>
 using namespace std;
+#define MAX 1000000000
+
 int main() {
 
 }
 
 /*
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    vector<int> v, back;
-    int N, input;
-
-    cin >> N;
-
-    for(int i = 0; i < N; i++) {
-        cin >> input;
-        v.push_back(input);
-        back.push_back(input);
+    int n;
+    cin >> n;
+    vector<int> v(n); //원본 벡터
+    // 입력
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    vector<int> cv(v); // 원본 벡터를 복사하여 중복 된 수를 제거하고 정렬을 시행 할 벡터.
+    sort(cv.begin(), cv.end()); //오름차순 정렬
+    // 중복 제거
+    cv.erase(unique(cv.begin(), cv.end()), cv.end());
+    for (int i = 0; i < n; i++)
+    {
+        // i번째 요소값의 위치 it
+        auto it = lower_bound(cv.begin(), cv.end(), v[i]);
+        // it에서 cv벡터의 시작 주소값을 빼준 값이 답
+        cout << it - cv.begin() << ' ';
     }
 
-
-    sort(v.begin(), v.end());
-    v.erase(unique(v.begin(), v.end()), v.end());
-
-    for(int i = 0; i < N; i++) {
-        printf("%d ", lower_bound(v.begin(), v.end(), back[i]) - v.begin());
-    }
-
+    return 0;
 }
 */
 
